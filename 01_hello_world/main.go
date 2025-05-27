b43d2a45e7058104a96306db8c8c84bac228ee7f
Naga Teja Gottigundala <112571523+ngottigu@users.noreply.github.com>
2025-05-27 18:18:04 -0500

    Update main.go

--- 01_hello_world/main.go
+++ 01_hello_world/main.go
@@ -1,5 +1,10 @@
 // Instructions:
 // Write a program that prints "Hello, World!" to the console.
 // Try to recall the syntax from memory.
+package main
+imort fmt
 
+func main(){
+  fmt.Println("Hello, Golang)
+}
 
